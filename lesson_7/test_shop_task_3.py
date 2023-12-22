4dd67a2a70bbf34eae279876cc8c26e3c551f797
Dmitry <dmitry030595@gmail.com>
2023-12-22 13:27:56 +0600

    seventh_hw_1

--- lesson_7/test_shop_task_3.py
+++ lesson_7/test_shop_task_3.py
@@ -19,30 +19,22 @@ info = UserInfo(driver)
 total = TotalPrice(driver)
 
 
-def test_reg():
+def test_total_price():
     reg.input_username()
     reg.input_password()
     reg.login_button_click()
 
-
-def test_main_page():
     main.add_backpack()
     main.add_t_shirt()
     main.add_onesie()
 
-
-def test_cart():
     cart.go_to_cart()
     cart.checkout_button_click()
 
-
-def test_user_info():
     info.input_first_name()
     info.input_last_name()
     info.input_postal_code()
     info.continue_button_click()
 
-
-def test_total_price():
     assert total.get_total_price()[7:] == '$58.29', 'total is not 58.29'
     driver.quit()
